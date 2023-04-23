5ac5e65300f06417dca4b656b850f6c5704f5eb5
anxuan2 <tnnddnguyendinhdo@Air-de-TNNDD.lan>
2023-04-23 20:17:41 +0200

    change names

--- main.py
+++ main.py
@@ -4,24 +4,26 @@ from nodule_tree import predict_nodule
 st.title("Outil pour l'aide dans la prise en charge thérapeutique des nodules thyroïdiens")
 col1, col2, col3 = st.columns(3)
 
+
 with col1:
-    tsh = st.number_input('TSH', 0.00, 200.00)
+    tsh = st.number_input('TSH (mUI/L)', 0.00, 200.00)
 with col2:
-    scinti = st.selectbox('Scinti', ('Oui (nodule autonome)', 'Non (pas de nodule autonome)', 'Non fait'))
+    scinti = st.selectbox('Scintigraphie', ("Présence d'un nodule autonome", 'Pas de nodule autonome', 'Non fait'))
 
 with col3:
-    adeno = st.selectbox('Adeno', ('cN0', 'cN1'))
+    adeno = st.selectbox('Adénopathies cervicales centrales', ('cN0', 'cN1'))
 
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
-    eutirads = st.selectbox('Eutirads', ('2', '3', '4', '5'))
+    eutirads = st.selectbox('Score EU-TIRADS', ('2', '3', '4', '5'))
 with col2:
-    cyto = st.selectbox('Cyto', ('I', 'II', 'III', 'IV', 'V', 'VI', 'non fait'))
+    cyto = st.selectbox('Cytologie : Score Bethesda', ('I', 'II', 'III', 'IV', 'V', 'VI', 'non fait'))
 with col3:
-    tnod = st.number_input('Tnod', 0, 1000)
+    tnod = st.number_input('Taille du nodule (mm)', 0, 1000)
 with col4:
-    histo = st.selectbox('Histo', ('Oui, carcinome papillaire', 'Non, bénin', 'Non fait'))
+    histo = st.selectbox("Histologie (lors de l'examen extemporané)",
+                         ("En faveur d'un carcinome papillaire", "En faveur d'un nodule bénin", 'Non fait'))
 
 if st.button('Run'):
     st.title(predict_nodule(tsh, scinti, adeno, eutirads, cyto, tnod, histo))
