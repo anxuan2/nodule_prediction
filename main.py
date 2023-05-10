9ea297a0733fb742206dd48e5de90a5c480e6a05
anxuan2 <tnnddnguyendinhdo@Air-de-TNNDD.lan>
2023-05-11 00:11:48 +0200

    add tree

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
-from nodule_tree import predict_nodule
-from nodule_tree_v2 import predict_nodule_v2
+from nodule_tree import predict_nodule_v2
+from tree_vizualization import create_nodule_tree
+from nodule_tree_v2 import predict_nodule_v3
 
 st.title("Outil pour l'aide dans la prise en charge thérapeutique des nodules thyroïdiens")
 
@@ -8,28 +9,28 @@ col1, col2 = st.columns(2)
 with col1:
     tsh = st.number_input('TSH (mUI/L)', 0.00, 200.00)
     tnod = st.number_input('Taille du nodule (mm)', 0, 1000)
+
     scinti = st.selectbox('Scintigraphie', ('Non fait', "Présence d'un nodule autonome", 'Pas de nodule autonome'))
     nod_bilat = st.selectbox('Nodule bilatéreaux', ('Non', 'Oui, nodules bilatéraux compressifs',
-                                                    'Oui, nodules bilatéraux non compressifs'))
+                                                   'Oui, nodules bilatéraux non compressifs'))
     cancer_bilat = st.selectbox('Cancer bilatéral ou Cancer isthmique', ('Non', 'Oui'))
 
 with col2:
     eutirads = st.selectbox('Score EU-TIRADS', (2, 3, 4, 5))
-    cyto = st.selectbox('Cytologie : Score Bethesda', ('I', 'II', 'III', 'IV', 'V', 'VI', 'Non fait'))
     adeno = st.selectbox('Adénopathies cervicales', ('cN0', 'cN1a', 'cN1b'))
+
+    cyto = st.selectbox('Cytologie : Score Bethesda', ('I', 'II', 'III', 'IV', 'V', 'VI', 'Non fait'))
     enceinte = st.selectbox('Femme enceinte / Projet grossesse / Nodule compressif', ('Non', 'Oui'))
     histo = st.selectbox("Histologie (lors de l'examen extemporané)",
                          ('Non fait', "En faveur d'un carcinome papillaire", "En faveur d'un nodule bénin"))
 
-
 if st.button('Run'):
-    prediction = predict_nodule_v2(scinti, tnod, eutirads, tsh, enceinte, cyto, nod_bilat,
-                                   adeno, cancer_bilat, histo)
+    prediction, color_n, color_e, style_n, style_e = predict_nodule_v3(tsh, tnod, eutirads,
+                                                                         adeno, cyto, nod_bilat, scinti,
+                                                                         enceinte, cancer_bilat, histo)
     st.title(prediction)
-
-st.write('')
-st.write('')
-st.write('')
+    st.graphviz_chart(create_nodule_tree(color_n, style_n, color_e, style_e))
 
 st.write("NB 1: L'algorithme de prise en charge se base sur le **consensus SFE-AFCE-SFMN 2022 sur la prise en charge des nodules thyroïdiens**.")
 st.write("NB 2: L'utilisation est dédiée pour la prise en charge des nodules thyroïdiens. Il n'est pas à utiliser pour les autres pathologies thyroïdiennes telles que l'hypo ou l'hyperthyroïdie par exemple.")
+
--- nodule_tree_v2.py
+++ nodule_tree_v2.py
@@ -1,32 +1,62 @@
-def predict_nodule_v2(scinti, tnod, eutirads, tsh, enceinte, cyto, nod_bilat,
-                      adeno, cancer_bilat, histo):
+import streamlit as st
+col1, col2 = st.columns(2)
+
+color_n = ['black' for _ in range(32)]
+style_n = ['solid' for _ in range(32)]
+color_e = ['black' for _ in range(36)]
+style_e = ['solid' for _ in range(36)]
+
+
+def predict_nodule_v3(tsh, tnod, eutirads, adeno, cyto, nod_bilat, scinti, enceinte,
+                      cancer_bilat, histo):
+    color_n[0] = 'red'
     if tsh <= 0.4:
-        return first_tree_left(scinti, enceinte, cyto, nod_bilat, tnod, adeno, cancer_bilat, histo)
-    else:
-        return first_tree_right(eutirads, tnod, tsh, scinti, enceinte, cyto, nod_bilat,
-                                adeno, cancer_bilat, histo)
+        color_e[0] = 'red'
+        color_n[1] = 'red'
+        return first_tree_left(scinti, enceinte, cyto, tnod, adeno, nod_bilat, cancer_bilat, histo)
+    elif tsh > 0.4:
+        color_e[1] = 'red'
+        color_n[2] = 'red'
+        return first_tree_right(eutirads, tnod, tsh, cyto, nod_bilat, adeno, cancer_bilat, histo, scinti, enceinte)
 
 
-def first_tree_right(eutirads, tnod, tsh, scinti, enceinte, cyto, nod_bilat, adeno,
-                     cancer_bilat, histo):
+def first_tree_right(eutirads, tnod, tsh, cyto, nod_bilat, adeno, cancer_bilat, histo, scinti, enceinte):
     if eutirads == 2 or (eutirads == 3 and tnod <= 20) or (eutirads == 4 and tnod <= 15) or (eutirads == 5 and tnod <= 10):
-        return 'Surveillance'
+        color_e[6] = 'red'
+        color_n[7] = 'red'
+        return 'Surveillance', color_n, color_e, style_n, style_e
     else:
+        color_e[7] = 'red'
+        color_n[8] = 'red'
         if tsh < 1:
-            return first_tree_left(scinti, enceinte, cyto, nod_bilat, tnod, adeno, cancer_bilat, histo)
+            color_e[8] = 'red'
+            color_n[1] = 'red'
+            return first_tree_left(scinti, enceinte, cyto, tnod, adeno, nod_bilat, cancer_bilat, histo)
         else:
+            color_e[10] = 'red'
+            color_n[9] = 'red'
             return second_tree(cyto, nod_bilat, tnod, adeno, cancer_bilat, histo)
 
 
-def first_tree_left(scinti, enceinte, cyto, nod_bilat, tnod, adeno, cancer_bilat, histo):
+def first_tree_left(scinti, enceinte, cyto, tnod, adeno, nod_bilat, cancer_bilat, histo):
     if scinti == "Présence d'un nodule autonome":
+        color_e[2] = 'red'
+        color_n[3] = 'red'
         if enceinte == 'Oui':
-            return 'Chirurgie'
+            color_e[4] = 'red'
+            color_n[5] = 'red'
+            return 'Chirurgie', color_n, color_e, style_n, style_e
         elif enceinte == 'Non':
-            return 'Iode radioactif'
+            color_e[5] = 'red'
+            color_e[6] = 'red'
+            return 'Iode radioactif', color_n, color_e, style_n, style_e
     elif scinti == 'Non fait':
-        return 'Faire scintigraphie'
+        color_e[3] = 'red'
+        color_n[4] = 'red'
+        return 'Faire scintigraphie', color_n, color_e, style_n, style_e
     elif scinti == 'Pas de nodule autonome':
+        color_e[9] = 'red'
+        color_n[9] = 'red'
         return second_tree(cyto, nod_bilat, tnod, adeno, cancer_bilat, histo)
 
 #######################################################################################################################
@@ -34,32 +64,56 @@ def first_tree_left(scinti, enceinte, cyto, nod_bilat, tnod, adeno, cancer_bilat
 
 def second_tree(cyto, nod_bilat, tnod, adeno, cancer_bilat, histo):
     if cyto == 'II':
+        color_e[13] = 'red'
+        color_n[12] = 'red'
         return second_tree_left(nod_bilat)
     elif cyto == 'I' or cyto == 'III':
-        return 'Contrôle de la cytoponction'
+        color_e[11] = 'red'
+        color_n[10] = 'red'
+        return 'Contrôle de la cytoponction', color_n, color_e, style_n, style_e
     elif cyto == 'IV' or cyto == 'V' or cyto == 'VI':
+        color_e[12] = 'red'
+        color_n[11] = 'red'
         return second_tree_right(nod_bilat, tnod, cyto, adeno, cancer_bilat, histo)
     elif cyto == 'Non fait':
-        return 'faire cytoponction'
+        color_e[14] = 'red'
+        color_n[13] = 'red'
+        return 'faire cytoponction', color_n, color_e, style_n, style_e
 
 
 def second_tree_left(nod_bilat):
     if nod_bilat == 'Oui, nodules bilatéraux compressifs':
-        return 'Thyroïdectomie totale'
+        color_e[16] = 'red'
+        color_n[15] = 'red'
+        return 'Thyroïdectomie totale', color_n, color_e, style_n, style_e
     elif nod_bilat == 'Oui, nodules bilatéraux non compressifs' or nod_bilat == 'Non':
-        return 'Surveillance'
+        color_e[15] = 'red'
+        color_n[14] = 'red'
+        return 'Surveillance', color_n, color_e, style_n, style_e
 
 
 def second_tree_right(nod_bilat, tnod, cyto, adeno, cancer_bilat, histo):
     if nod_bilat == 'Non':
+        color_e[20] = 'red'
+        color_n[19] = 'red'
         return third_tree(adeno, cyto, tnod, cancer_bilat, histo)
     elif nod_bilat == 'Oui, nodules bilatéraux compressifs' or nod_bilat == 'Oui, nodules bilatéraux non compressifs':
+        color_e[17] = 'red'
+        color_n[16] = 'red'
         if (cyto == 'IV' and tnod > 40) or (cyto == 'V' and tnod > 20) or (cyto == 'VI' and tnod > 20):
+            color_e[18] = 'red'
+            color_n[17] = 'red'
             if adeno == 'cN0':
-                return 'Thyroïdectomie totale'
+                color_e[19] = 'red'
+                color_n[18] = 'red'
+                return 'Thyroïdectomie totale', color_n, color_e, style_n, style_e
             else:
+                color_e[35] = 'red'
+                color_n[19] = 'red'
                 return third_tree(adeno, cyto, tnod, cancer_bilat, histo)
         else:
+            color_e[20] = 'red'
+            color_n[19] = 'red'
             return third_tree(adeno, cyto, tnod, cancer_bilat, histo)
 
 #######################################################################################################################
@@ -67,38 +121,62 @@ def second_tree_right(nod_bilat, tnod, cyto, adeno, cancer_bilat, histo):
 
 def third_tree(adeno, cyto, tnod, cancer_bilat, histo):
     if adeno == 'cN0':
+        color_e[24] = 'red'
+        color_n[22] = 'red'
         return third_tree_left(cyto, tnod, cancer_bilat, histo)
     elif adeno == 'cN1a':
-        return 'Thyroïdectomie totale + curage central homolateral'
+        color_e[23] = 'red'
+        color_n[21] = 'red'
+        return 'Thyroïdectomie totale + curage central homolateral', color_n, color_e, style_n, style_e
     elif adeno == 'cN1b':
-        return 'Thyroïdectomie totale + curage latéral III et IV + curage central homolatéra'
+        color_e[22] = 'red'
+        color_n[20] = 'red'
+        return 'Thyroïdectomie totale + curage latéral III et IV + curage central homolatéral', color_n, color_e, style_n, style_e
 
 
 def third_tree_left(cyto, tnod, cancer_bilat, histo):
     if cyto == 'I' or cyto == 'II' or cyto == 'III' or cyto == 'IV':
-        return 'Loboisthmectomie'
+        color_e[25] = 'red'
+        color_n[24] = 'red'
+        return 'Loboisthmectomie', color_n, color_e, style_n, style_e
     if cyto == 'V' or cyto == 'VI':
+        color_e[26] = 'red'
+        color_n[23] = 'red'
         return third_tree_central(tnod, cancer_bilat, histo)
 
 
 def third_tree_central(tnod, cancer_bilat, histo):
     if cancer_bilat == 'Oui':
-        return 'Thyroidectomie totale + curage central bilatéral'
+        color_e[28] = 'red'
+        color_n[26] = 'red'
+        return 'Thyroidectomie totale + curage central bilatéral', color_n, color_e, style_n, style_e
     elif cancer_bilat == 'Non':
+        color_e[27] = 'red'
+        color_n[25] = 'red'
         return third_tree_right(tnod, histo)
 
 
 def third_tree_right(tnod, histo):
     if tnod < 20:
-        return 'Loboisthmectomie'
+        color_e[29] = 'red'
+        color_n[27] = 'red'
+        return 'Loboisthmectomie', color_n, color_e, style_n, style_e
     elif tnod < 40:
-        return 'Thyroïdectomie totale'
+        color_e[30] = 'red'
+        color_n[28] = 'red'
+        return 'Thyroïdectomie totale', color_n, color_e, style_n, style_e
     elif tnod >= 40:
+        color_e[31] = 'red'
+        color_n[29] = 'red'
         if histo == "En faveur d'un carcinome papillaire":
-            return 'Thyroïdectomie totale et Curage récurentiel homolatéral'
+            color_e[34] = 'red'
+            color_n[21] = 'red'
+            return 'Thyroïdectomie totale et Curage récurentiel homolatéral', color_n, color_e, style_n, style_e
         elif histo == "En faveur d'un nodule bénin":
-            return 'Thyroïdectomie totale'
+            color_e[33] = 'red'
+            color_n[28] = 'red'
+            return 'Thyroïdectomie totale', color_n, color_e, style_n, style_e
         elif histo == 'Non fait':
-            return 'Thyroïdectomie totale et Examen extemporané'
-
-
+            color_e[32] = 'red'
+            color_n[30] = 'red'
+            return 'Thyroïdectomie totale et Examen extemporané', color_n, color_e, style_n, style_e
